d28dd720cefab33b9db73c7a0533aac0b07a2a5d
Adriano Barroso <wiermann@broou.com>
2018-01-22 20:22:29 -0200

    clean up routes, add root_path and change back

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-
-  get 'bookings/index'
-
-  get 'bookings/show'
+  root 'motels#index'
 
   resources :motels, only: [:index, :show] do
     resources :rooms, only: [:index, :show]
     resources :bookings, only: [:index]
   end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
